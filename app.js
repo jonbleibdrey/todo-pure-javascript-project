4ea2a25cdfb5e8238d705afd21f1a8dc0ac226ad
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-09-24 16:32:29 -0400

    added styling to buttons

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function addTodo(event) {
   // delete button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("complete-btn");
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   // append to list
   todoList.appendChild(todoDiv);
