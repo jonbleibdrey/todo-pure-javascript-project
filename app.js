9420732b210326772cdb065f87503ef06b00572d
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-09-23 22:00:23 -0400

    added event listeners, functions, and selectors

---
+++ app.js
@@ -0,0 +1,20 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(event) {
+  //prevent form from submitting
+  event.preventDefault();
+  //todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+}
