004d70f1b0f3a190641eabb5da289ab33a0f5759
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-09-27 12:47:52 -0400

    added check off list

--- app.js
+++ app.js
@@ -42,4 +42,10 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.remove();
   }
+
+  //check mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
