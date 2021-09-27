be580ebfe069376a8b15fb86291acddc56429f19
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-09-27 12:44:12 -0400

    added delete functionality

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(event) {
@@ -31,5 +32,14 @@ function addTodo(event) {
   // append to list
   todoList.appendChild(todoDiv);
   //clear todo vlaue
-  todoInput.value = ""
+  todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  //delett todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
 }
