1b781a37b1bc77ade588e4811958fd8090541158
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-10-06 20:36:45 -0400

    added localstorage

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ function addTodo(event) {
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
+  //Add todo to local storage
+  saveLocalTodo(todoInput.value);
   // check mark button
   const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -70,13 +72,26 @@ function filterTodo(e) {
           todo.style.display = "none";
         }
         break;
-        case "uncompleted":
-          if(!todo.classList.contains("completed")){
-            todo.style.display = "flex";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
     }
   });
+}
+
+function saveLocalTodo(todo) {
+  //check if i have something already
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
